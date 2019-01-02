b6db22a6ed1994aaa0c2c8c37acdac212a60446c
Derk Schmitt <Derk@Jessicas-MacBook-Pro-2.local>
2019-01-02 07:29:45 -0500

    Add auth, Transaction model and Type model

---
+++ database/seeds/TypesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('types')->insert([
+            'name' => 'Revenue',
+            'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('types')->insert([
+            'name' => 'Expense',
+            'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
