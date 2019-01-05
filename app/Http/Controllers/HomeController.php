c396b147b3d99e9bb2ea2fdb958341ac79b87435
Derk Schmitt <Derk@Jessicas-MacBook-Pro-2.local>
2019-01-05 15:49:47 -0500

    Update index method to get all incomes and send it to the view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Income;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $incomes = Income::all();
+        return view('home', compact('incomes'));
     }
 }
